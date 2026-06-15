e3c6aede347b035a181d562919f8dc7af3e277c9
José Felipe <jfelipe.jfss@gmail.com>
2026-06-15 15:49:41 -0300

    Adicionado mensagem de erro visível ao usuário, e separação da mensagem de erro por meio de trait

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace Alura\Mvc\Controller;
 
+use Alura\Mvc\Helper\FlashMessageTrait;
+
 class LoginController implements Controller
 {
+    use FlashMessageTrait;
+
     private \PDO $pdo;
 
     public function __construct()
@@ -27,18 +31,19 @@ class LoginController implements Controller
         $userData = $statement->fetch(\PDO::FETCH_ASSOC);
         $correctPassword = password_verify($password, $userData['password'] ?? '');
 
-        if (password_needs_rehash($userData['password'], PASSWORD_ARGON2ID)){
-            $statement = $this->pdo->prepare('UPDATE users SET password = ? WHERE id = ?');
-            $statement->bindValue(1, password_hash($password, PASSWORD_ARGON2ID));
-            $statement->bindvalue(2, $userData['id']);
-            $statement->execute();
-        }
-
         if ($correctPassword) {
+            if (password_needs_rehash($userData['password'], PASSWORD_ARGON2ID)) {
+                $statement = $this->pdo->prepare('UPDATE users SET password = ? WHERE id = ?');
+                $statement->bindValue(1, password_hash($password, PASSWORD_ARGON2ID));
+                $statement->bindValue(2, $userData['id']);
+                $statement->execute();
+            }
+
             $_SESSION['logado'] = true;
             header('Location: /');
         } else {
-            header('Location: /login?sucesso=0');
+            $this->addErrorMessage ($_SESSION['error_message'] = 'Usuário ou Senha inválidos!');
+            header('Location: /login');
         }
     }
 }
--- src/Controller/VideoListController.php
+++ src/Controller/VideoListController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Alura\Mvc\Controller;
 
+use Alura\Mvc\Helper\HtmlRendererTrait;
 use Alura\Mvc\Repository\VideoRepository;
 
 class VideoListController implements Controller
@@ -11,10 +12,12 @@ class VideoListController implements Controller
     public function __construct(private VideoRepository $videoRepository)
     {
     }
+    
+    use HtmlRendererTrait;
 
     public function processaRequisicao(): void
     {
         $videoList = $this->videoRepository->all();
-        require_once __DIR__ . '/../../views/video-list.php';
+        echo $this->renderTemplate('video-list', ['video-list' => $videoList]);
     }
 }
